6b49e53ef0622d2e6a5f6e8b4077d4c12d221e48
Dāvis Šķipsna <skipsna.davis@gmail.com>
2020-07-14 14:55:52 +0300

    Created displayEmployees and renderEmployeeHTML functions

--- js/employee.js
+++ js/employee.js
@@ -12,3 +12,40 @@ const fetchEmployees = async () => {
   );
   return await response.json();
 };
+/**
+ * @function - Renders HTML structure for employee object.
+ * @param {Object} employee - Current employee object.
+ * @param {Number} index - Position of array.
+ */
+const renderEmployeeHTML = (employee, index) => {
+  return `<div class="card" data-index="${index}">
+    <div class="card-img-container">
+        <img class="card-img" src="${employee.picture.large}" alt="profile picture">
+    </div>
+    <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+        <p class="card-text">${employee.email}</p>
+        <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+    </div>
+</div>`;
+};
+
+/**
+ * @function - Renders employees to @const {galleryCont}.
+ * @param {Array} arr - Array of employees objects.
+ * @function {renderEmployeeHTML} - Renders HTML for every employee.
+ */
+const displayEmployees = (arr) => {
+  const galleryCont = document.getElementById("gallery");
+  galleryCont.innerHTML = ``;
+  arr.forEach((employee, index) => {
+    galleryCont.innerHTML += renderEmployeeHTML(employee, index);
+  });
+};
+
+fetchEmployees()
+  .then((res) => {
+    employees = res.results;
+    displayEmployees(employees);
+  })
+  .catch((err) => console.log(err));
